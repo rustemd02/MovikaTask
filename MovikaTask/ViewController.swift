cee6d9f00fee4ac684b33712f14e76c44122d514
rustemd02 <rustemd02@mail.ru>
2022-06-09 15:38:57 +0300

    Initial Commit

---
+++ MovikaTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovikaTask
+//
+//  Created by Рустем on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
